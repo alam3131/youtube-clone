e5347bb8258378a8a63c60c5af73cb069580b29c
alam3131 <lamanthony678@gmail.com>
2024-08-08 16:14:34 -0500

    finished convert videos hosted on GCS

--- video-processing-service/src/index.ts
+++ video-processing-service/src/index.ts
@@ -1,31 +1,60 @@
 import express from "express";
-import ffmpeg from "fluent-ffmpeg";
+import { downloadRawVideo, 
+        setupDirectories, 
+        uploadProcessedVideo, 
+        convertVideo, 
+        deleteProcessedVideo, 
+        deleteRawVideo } from "./storage";
+
+setupDirectories();
 
 const app = express();
 app.use(express.json());
 
 // Post methods happens when client makes a request to process a video
-app.post("/process-video", (req, res) => {
-    // Get path of the input video file from the request body
-    const inputFilePath = req.body.inputFilePath;
-    const outputFilePath = req.body.outputFilePath;
-
-    // Checks if the file paths are valid and returns a status if invalid
-    if (!inputFilePath || !outputFilePath) {
-        res.status(400).send("Bad Request: Missing file path.");
+app.post("/process-video", async (req, res) => {
+    // Get the bucket and filename from the Cloud Pub/Sub message
+    let data;
+    try {
+        const message = Buffer.from(req.body.message.data, 'base64').toString('utf8');
+        data = JSON.parse(message);
+        if (!data.name) {
+            throw new Error('Invalid message payload received.');
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(400).send('Bad Request: missing filename.');
     }
 
-    // Video processing
-    ffmpeg(inputFilePath)
-        .outputOptions("-vf", "scale=-1:360") // 360p
-        .on("end", () => { // Sends status 200 and message on successful completion
-            res.status(200).send("Processing finished successfully.")
-        })
-        .on("error", (err) => { // Sends error message and status 500 on error
-            console.log(`An error occurred: ${err.message}`);
-            res.status(500).send(`Internal Server Error: ${err.message}`);
-        })
-        .save(outputFilePath); // Saves the video output to the outputFilePath
+    const inputFileName = data.name;
+    const outputFileName = `processed-${inputFileName}`;
+
+    // Download the raw video from Cloud Storage
+    await downloadRawVideo(inputFileName);
+
+    // Convert the video to 360p
+    try {
+        await convertVideo(inputFileName, outputFileName);
+    } catch (err) {
+        await Promise.all([
+            deleteRawVideo(inputFileName),
+            deleteProcessedVideo(outputFileName)
+        ]);
+
+        console.error(err);
+        return res.status(500).send('Internal Server Error: video processing failed.');
+    }
+
+    // Upload the processed video to Cloud Storage
+    await uploadProcessedVideo(outputFileName);
+
+    await Promise.all([
+        deleteRawVideo(inputFileName),
+        deleteProcessedVideo(outputFileName)
+    ]);
+
+    return res.status(200).send('Processing finished successfully')
+
 });
 
 // Port number can be provided as an environment variable
